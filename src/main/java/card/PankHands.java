06ba6b050b4b76f71b7e843aed859ecf3e4a524a
luhaojian1 <962671114@qq.com>
2019-08-16 17:21:30 +0800

    divide card numbers when given 2H 3D 4S 8H TH

--- src/main/java/card/PankHands.java
+++ src/main/java/card/PankHands.java
@@ -1,5 +1,8 @@
 package card;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PankHands {
     public String compareCards(String[] player1, String[] player2) {
         int card1 = Integer.parseInt(player1[0].substring(0, 1));
@@ -9,5 +12,14 @@ public class PankHands {
         } else if (card1 < card2) {
             return "player2";
         } else return "deuce";
+    }
+
+    public Map<String, Integer> divideCards(String[] cards) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String card : cards) {
+            String key = card.substring(0, 1);
+            map.merge(key, 1, Integer::sum);
+        }
+        return map;
     }
 }
--- src/test/java/PankHandsTest.java
+++ src/test/java/PankHandsTest.java
@@ -1,9 +1,11 @@
 import card.PankHands;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class PankHandsTest {
+class PankHandsTest {
     @Test
     void should_player2_win_given_player1_2C_and_player2_3C() {
         PankHands pankHands = new PankHands();
@@ -17,4 +19,22 @@ public class PankHandsTest {
         //then
         assertEquals("player2", result);
     }
+
+    @Test
+    void should_divide_card_numbers_when_given_2H_3D_4S_8H_TH() {
+        //given
+        PankHands pankHands = new PankHands();
+        String[] players = {"2H", "3D", "4S", "8H", "TH"};
+
+        //when
+        Map<String, Integer> cards = pankHands.divideCards(players);
+
+        //given
+        assertEquals(1, cards.get("T"));
+        assertEquals(1, cards.get("2"));
+        assertEquals(1, cards.get("3"));
+        assertEquals(1, cards.get("4"));
+        assertEquals(1, cards.get("8"));
+    }
+
 }
