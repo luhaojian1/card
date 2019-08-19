5a8a88b052e5378c31472a9a0030ff7d036fad85
luhaojian1 <962671114@qq.com>
2019-08-19 12:54:21 +0800

    refactor code and extract methods

--- src/main/java/card/PankHands.java
+++ src/main/java/card/PankHands.java
@@ -5,52 +5,80 @@ import java.util.stream.Collectors;
 
 public class PankHands {
     public String compareCards(String[] player1, String[] player2) {
-        Map<String, Integer> cards1 = divideCards(player1);
-        Map<String, Integer> cards2 = divideCards(player2);
-        List<Integer> values1 = getValues(cards1);
-        List<Integer> values2 = getValues(cards2);
-        int cardsLevel1 = findCardsLevel(player1, values1);
-        int cardsLevel2 = findCardsLevel(player2, values2);
+        Map<String, List<Integer>> cards1 = designPankCards(player1);
+        Map<String, List<Integer>> cards2 = designPankCards(player2);
+        return findWinners(cards1, cards2);
 
+    }
+
+    private String findWinners(Map<String, List<Integer>> cards1, Map<String, List<Integer>> cards2) {
+        List<Integer> values1 = cards1.get("values");
+        List<Integer> values2 = cards2.get("values");
+        int cardsLevel1 = cards1.get("level").get(0);
+        int cardsLevel2 = cards2.get("level").get(0);
         if (cardsLevel1 > cardsLevel2) {
             return "player1";
         } else if (cardsLevel1 < cardsLevel2) {
             return "player2";
         } else {
-            List<Integer> cardNumbers1 = findCarNumbers(cards1);
-            List<Integer> cardNumbers2 = findCarNumbers(cards2);
-
+            List<Integer> cardNumbers1 = cards1.get("carNumbers");
+            List<Integer> cardNumbers2 = cards2.get("carNumbers");
             if (cardsLevel1 == Level.Two_Pairs.code()) {
-                List<Integer> twoPairCards2 = findTwoPairCarNumbers(cardNumbers2, values2);
-                List<Integer> twoPairCards1 = findTwoPairCarNumbers(cardNumbers1, values1);
-                String winner = findMaxValuePlayer(twoPairCards1, twoPairCards2);
-                if (winner.equals("deuce")) {
-                    int singleValue1 = cardNumbers1.stream().filter(carNumber -> !twoPairCards1.contains(carNumber)).findFirst().orElse(0);
-                    int singleValue2 = cardNumbers2.stream().filter(carNumber -> !twoPairCards1.contains(carNumber)).findFirst().orElse(0);
-                    if (singleValue1 > singleValue2) {
-                        return "player1";
-                    } else if (singleValue1 < singleValue2) {
-                        return "player2";
-                    } else return "deuce";
-                }
-                return winner;
+                return compareTwoPairs(values1, values2, cardNumbers1, cardNumbers2);
             } else if (cardsLevel1 == Level.Pair.code()) {
-                List<Integer> cardNum1 = findFilterCarNumbers(cardNumbers1, values1);
-                List<Integer> cardNum2 = findFilterCarNumbers(cardNumbers2, values2);
-                return findMaxValuePlayer(cardNum1, cardNum2);
+                return comparePairs(values1, values2, cardNumbers1, cardNumbers2);
+            } else if (cardsLevel1 == Level.Three_of_a_Kind.code() || cardsLevel1 == Level.Full_House.code() || cardsLevel1 == Level.Four_of_a_Kind.code()) {
+                return compareThreeOrFourKinds(values1, values2);
             } else {
                 return findMaxValuePlayer(cardNumbers1, cardNumbers2);
             }
-
         }
-
     }
 
-    private int findCardsLevel(String[] playingCards, List<Integer> values) {
+    private String compareThreeOrFourKinds(List<Integer> values1, List<Integer> values2) {
+        int maxValues1 = values1.stream().max(Comparator.naturalOrder()).orElse(0);
+        int maxValues2 = values2.stream().max(Comparator.naturalOrder()).orElse(0);
+        return compareValueAndFindWinners(maxValues1, maxValues2);
+    }
+
+    private String comparePairs(List<Integer> values1, List<Integer> values2, List<Integer> cardNumbers1, List<Integer> cardNumbers2) {
+        List<Integer> cardNum1 = findFilterCarNumbers(cardNumbers1, values1);
+        List<Integer> cardNum2 = findFilterCarNumbers(cardNumbers2, values2);
+        return findMaxValuePlayer(cardNum1, cardNum2);
+    }
+
+    private String compareTwoPairs(List<Integer> values1, List<Integer> values2, List<Integer> cardNumbers1, List<Integer> cardNumbers2) {
+        List<Integer> twoPairCards2 = findTwoPairCarNumbers(cardNumbers2, values2);
+        List<Integer> twoPairCards1 = findTwoPairCarNumbers(cardNumbers1, values1);
+        String winner = findMaxValuePlayer(twoPairCards1, twoPairCards2);
+        if (winner.equals("deuce")) {
+            int singleValue1 = cardNumbers1.stream().filter(carNumber -> !twoPairCards1.contains(carNumber)).findFirst().orElse(0);
+            int singleValue2 = cardNumbers2.stream().filter(carNumber -> !twoPairCards1.contains(carNumber)).findFirst().orElse(0);
+            return compareValueAndFindWinners(singleValue1, singleValue2);
+        }
+        return winner;
+    }
+
+    private String compareValueAndFindWinners(int value1, int value2) {
+        if (value1 > value2) {
+            return "player1";
+        } else if (value1 < value2) {
+            return "player2";
+        } else return "deuce";
+    }
+
+    private Map<String, List<Integer>> designPankCards(String[] playingCards) {
+        Map<String, Integer> cards = divideCards(playingCards);
+        Map<String, List<Integer>> pankCards = new HashMap<>();
+        List<Integer> cardNumbers = findCarNumbers(cards);
+        pankCards.put("carNumbers", cardNumbers);
+        List<Integer> values = getValues(cards);
+        pankCards.put("values", values);
         boolean isStraight = isStraightCards(values);
         boolean isFlush = isFlushCards(playingCards);
-        return judgeCardsLevel(isFlush, isStraight, values);
-
+        int level = judgeCardsLevel(isFlush, isStraight, values);
+        pankCards.put("level", Collections.singletonList(level));
+        return pankCards;
     }
 
     private List<Integer> findFilterCarNumbers(List<Integer> cardNumbers, List<Integer> values) {
